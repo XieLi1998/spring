5e1ba881f10ca1bdd4043b24392fe60a150b05ac
xieli <2524546110@qq.com>
2021-01-06 17:22:31 +0800

    leetcode

---
+++ src/leetcode/HOT_100/No2_两数相加.java
@@ -0,0 +1,54 @@
+package leetcode.HOT_100;
+
+/**
+ * Created by xieli on 2021/1/6.
+ */
+public class No2_两数相加 {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(-1);
+        ListNode cur = pre;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+            cur = cur.next;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry == 1) {
+            cur.next = new ListNode(carry);
+        }
+        return pre.next;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
